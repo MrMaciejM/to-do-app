b754b4764c9fe6a7716c6df02d058b2276a39e6b
maciej <63735702+MrMaciejM@users.noreply.github.com>
2022-12-09 14:30:35 +0000

    backing up files and testing code for localStorage

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var inputBtn = document.getElementById("inputBtn");
 var toDoArr = [];
 var todoArrCopy = [];
 
+var getStoredItems = localStorage.getItem("todos");
+//console.log(getStoredItems);
+
 var submitBtn = document.getElementById("submit");
 var inputBtn = document.getElementById("inputBtn");
 var deleteAllBtn = document.getElementById("deleteAll");
@@ -36,37 +39,60 @@ submitBtn.addEventListener("click", (e) => {
   if (submitVal === undefined || submitVal === "" || submitVal === " " || submitVal === null) {
     //
     alert(
-      "You cannot add an empty item... \nI mean, c'mon... :("
+      "You cannot add an empty item... :("
     );
     inputBtn.value = "";
     return;
   }
 
-  // adds item to page
+  // adds item(s) to page, their respective buttons and classes
   var li = document.createElement("li");
+  var delBtn = document.createElement("button");
   li.setAttribute("class", "center");
+  delBtn.setAttribute("class", "delItemBtn");
   li.innerText = submitVal;
+  li.insertAdjacentHTML(
+    "beforeend",
+    `<button class="delItemBtn">Delete</button>`
+  );
   document.body.append(li);
+
   var liValue = li.innerText; // list item string to be passed into array
 
   // add string item to array
   toDoArr.push(`${liValue}`); // ["11"];
+  //console.log(toDoArr);
 
-  // save to local storage
-  localStorage.setItem("todos", toDoArr);
+  // push to copy array / save to localStorage - needs fixing
+  // get OG array with getItem
+  // getStoredItems
+  // add array to getStorageOG
+  arr = getStoredItems;
+
+  arr = JSON.parse(localStorage.getItem("todos")) || "[]";
+  console.log(arr);
+  arr.push(toDoArr);
+  localStorage.setItem("todos", JSON.stringify(getSavedItems));
+
+  //todoArrCopy.push(toDoArr);
+  // add new
+  //getOGArray.push(toDoArr);
+  // update
+  //localStorage.setItem("todos", toDoArr);
+
+  //toDoArr.push(todoArrCopy);
+  //localStorage.setItem("todos", todoArrCopy);
+  //console.log(getStoredItems);
 
   //reset input field
   inputBtn.value = "";
 });
+// save to local storage
+//localStorage.setItem("todos", todoArrCopy);
 
 // get items from localStorage
-var savedList = localStorage.getItem("todos");
-var savedListArr = savedList.split(",");
 
-for (var i = 0; i < savedListArr.length; i++) {
-  var li = document.createElement("li");
-  li.setAttribute("class", "center");
-  li.innerText = savedListArr[i];
-  document.body.append(li);
-  console.log(savedListArr);
-}
+//var savedList = localStorage.getItem("todos");
+//console.log("savedList");
+//console.log(savedList);
+//var savedListArr = savedList.split(",");
